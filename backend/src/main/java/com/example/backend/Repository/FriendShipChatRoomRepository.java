40107ad0234383b12c9518d56f676671f2fd54af
MianShang <napori03@naver.com>
2025-09-17 09:13:34 +0900

    Friendship ChatRoom Repository 추가

--- backend/src/main/java/com/example/backend/Repository/FriendShipChatRoomRepository.java
+++ backend/src/main/java/com/example/backend/Repository/FriendShipChatRoomRepository.java
@@ -5,5 +5,5 @@ import com.example.backend.Entity.FriendShipChatRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FriendShipChatRoomRepository extends JpaRepository<FriendShipChatRoom, Integer> {
-    
+
 }
